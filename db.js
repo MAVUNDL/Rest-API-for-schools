03ee905c4d645183d18fe831abdfab828f5b6f74
SgaxaMabhande <sikhumbuzobembe184@gmail.com>
2024-12-02 15:11:46 +0200

    checking

--- db.js
+++ db.js
@@ -3,13 +3,6 @@ require("dotenv").config();
 
 // establish connection
 const pool = new Pool(
-    // {
-    //     user: process.env.user,
-    //     host: process.env.host,
-    //     database: process.env.database,
-    //     password: process.env.password,
-    //     port: process.env.port,
-    // }
     {
         connectionString: process.env.connectionString,
         ssl: {
@@ -18,6 +11,8 @@ const pool = new Pool(
     }
 );
 
-console.log(process.env.connectionString);
+pool.connect()
+    .then(() => console.log('Connected to PostgreSQL database'))
+    .catch(err => console.error('Connection error', err.stack));
 
 module.exports = pool;
