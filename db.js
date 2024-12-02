ce12b6fc2b9f30ff1805de659c2c60522277315e
SgaxaMabhande <sikhumbuzobembe184@gmail.com>
2024-12-02 13:56:28 +0200

    first commit

---
+++ db.js
@@ -0,0 +1,18 @@
+const Pool = require('pg').Pool;
+require("dotenv").config();
+
+// establish connection
+const pool = new Pool(
+    // {
+    //     user: process.env.user,
+    //     host: process.env.host,
+    //     database: process.env.database,
+    //     password: process.env.password,
+    //     port: process.env.port,
+    // }
+    {
+        connectionString: process.env.connectionString
+    }
+);
+
+module.exports = pool;
